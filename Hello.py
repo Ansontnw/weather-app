cb98ea5dbaffc3cce9f187421c0f3ebfe7e07b57
Ansontnw <165770344+Ansontnw@users.noreply.github.com>
2024-05-06 11:24:12 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,13 +1,17 @@
 import streamlit as st
 import requests
 
-def fetch_weather_data(api_key, city, state):
-    url = f"http://api.weatherstack.com/current?access_key={api_key}&query={city},{state}"
+def fetch_weather_data(api_key, city):
+    url = f"http://api.weatherstack.com/current?access_key={api_key}&query={city}"
     response = requests.get(url)
     data = response.json()
     return data
 
-
+def fetch_location_data(api_key, latitude, longitude):
+    url = f"http://api.weatherstack.com/current?access_key={api_key}&query={latitude},{longitude}"
+    response = requests.get(url)
+    data = response.json()
+    return data
 
 def main():
     st.title("Hello!, Welcome to Weather App")
@@ -18,20 +22,16 @@ def main():
     if st.button("Search"):
         if city:
             try:
-                weather_data = fetch_weather_data(api_key='0f33509df08b7bea7f411f2e27c75430', city=city, state='StateName')
+                weather_data = fetch_weather_data(api_key='0f33509df08b7bea7f411f2e27c75430', city=city)
 
-
-
-                if 'success' in weather_data and not weather_data['success']:
+                if 'error' in weather_data:
                     st.write("Error:", weather_data['error']['info'])
-                elif 'current' in weather_data:
-                    st.write(f"Weather in {city}:")
+                else:
+                    st.write(f"Weather in {weather_data['location']['name']}, {weather_data['location']['region']}, {weather_data['location']['country']}:")
                     st.write(f"Temperature: {weather_data['current']['temperature']}°C")
                     st.write(f"Description: {weather_data['current']['weather_descriptions'][0]}")
                     st.write(f"Humidity: {weather_data['current']['humidity']}%")
                     st.write(f"Wind Speed: {weather_data['current']['wind_speed']} m/s")
-                else:
-                    st.write("Error: Unable to fetch weather data. Please try again later.")
 
             except Exception as e:
                 st.write("Error fetching weather data:", e)
