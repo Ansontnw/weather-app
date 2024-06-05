9ce02d8df1f8a03040c9556e49960824e096052b
Ansontnw <165770344+Ansontnw@users.noreply.github.com>
2024-06-05 10:58:47 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -65,6 +65,7 @@ def main():
                 weather_data = fetch_weather_data(weather_api_key, city)
                 forecast_data = fetch_forecast_data(weather_api_key, city)
 
+                # Check if the 'cod' field in the weather_data is not 200 (which means there was an error)
                 if weather_data.get('cod') != 200:
                     st.write("Error:", weather_data.get('message', 'Failed to retrieve data'))
                 else:
@@ -74,7 +75,8 @@ def main():
                     st.write(f"Description: {weather_data['weather'][0]['description'].capitalize()}")
                     st.write(f"Humidity: {weather_data['main']['humidity']}%")
                     st.write(f"Wind Speed: {weather_data['wind']['speed']} m/s")
-                    
+
+                    #Print Longitude and Latitude, UV Index
                     lat = weather_data['coord']['lat']
                     lon = weather_data['coord']['lon']
                     st.write(f"Latitude: {lat}")
@@ -84,29 +86,36 @@ def main():
                         st.write(f"UV Index: {uv_data['value']}")
                     else:
                         st.write("Failed to retrieve UV index data")
-                    
+                        
+                    #Use Longitude and Latidue to get the tide information
                     tide_data = fetch_tide_data(stormglass_api_key, lat, lon)
                     if 'data' in tide_data:
                         st.header("Tide Information:")
+                        
+                        # Start with empty lists to store tide times and tide height
                         tide_times = []
                         tide_heights = []
+                        
                         for tide in tide_data['data']:
                             tide_time = datetime.fromisoformat(tide['time'].replace('Z', '+00:00'))
                             tide_height = tide['height']
                             #st.write(f"Time: {tide_time.strftime('%Y-%m-%d %H:%M:%S')}, Height: {tide_height} meters")
                             tide_times.append(tide_time)
                             tide_heights.append(tide_height)
-
+                        
+                        # Time = Paksi X, Tide height = Paksi Y
                         tide_data_df = pd.DataFrame({'Time': tide_times, 'Tide Height': tide_heights})
                         st.line_chart(tide_data_df.set_index('Time'))
                     else:
                         st.write("Failed to retrieve tide data")
-
+                        
+                    # Check if the 'cod' field in the forcast_data is not 200 (which means there was an error)
                     if forecast_data.get('cod') != "200":
                         st.write("Error:", forecast_data.get('message', 'Failed to retrieve forecast data'))
                     else:
                         st.header("5-Day Forecast:")
-
+                        
+                        # Start with empty lists to store dates and temperatures
                         dates = []
                         temperatures = []
                         
@@ -121,7 +130,8 @@ def main():
                                 st.write("---")
                                 dates.append(dt)
                                 temperatures.append(forecast['main']['temp'])
-                                
+                        
+                        # Date = Paksi X, Temperature = Paksi Y        
                         data = pd.DataFrame({'Date':dates, 'Temperature':temperatures})
                         st.line_chart(data.set_index('Date'))
 
